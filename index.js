c4c3b7d75fff204a43ea3371dd48a9b777f329ef
mdsataide <mdsataide@gmail.com>
2019-07-15 16:04:08 -0300

    Aula 06 - Query e Route params

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World!" });
+// Query Params - ?name=marcelo
+// Route Params - /users/1
+// Request Body(post, put) - { 'name': 'Marcelo', 'email': 'mdsataide@gmail.com' }
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({
+    message: `Buscando o usuário com id: ${id}`
+  });
 });
 
 server.listen(3000);
